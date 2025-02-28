3203efd41c8e34b5eb6d427f5a02c6392e1f30db
Tim Jensen <tim.l.jensen@gmail.com>
2025-02-27 19:09:17 -0600

    Renaming variables to match fork name

--- scripts/5_mission/mission/MissionGameplay.c
+++ scripts/5_mission/mission/MissionGameplay.c
@@ -1,38 +1,40 @@
-modded class MissionGameplay extends MissionBase
+modded class MissionGameplay
 {
-	override void OnUpdate(float timeslice)
-	{
-		super.OnUpdate(timeslice);
+    override void OnUpdate(float timeslice)
+    {
+        super.OnUpdate(timeslice);
 
-		// Get player
-		autoptr PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
-		// Get input controller
-		autoptr Input zenInput = Input.Cast(GetGame().GetInput());
+        // Get player
+        autoptr PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
+        // Get input controller
+        autoptr Input chfInput = Input.Cast(GetGame().GetInput());
 
-		if (!player || !zenInput)
-			return;
+        if (!player || !chfInput)
+            return;
 
-		// If a menu is open stop here
-		if (GetGame().GetUIManager().GetMenu() != null || GetGame().GetUIManager().IsCursorVisible())
-		{
-			return;
-		}
+        // If a menu is open stop here
+        if (GetGame().GetUIManager().GetMenu() != null || GetGame().GetUIManager().IsCursorVisible())
+        {
+            return;
+        }
 
-		if (zenInput.LocalPress("UACHFSleep_RefreshConfig")) // Sends request to server to reload json config if player is admin
-		{
-			player.RequestServerConfigReload();
-		} else
-		if (zenInput.LocalPress("UACHFSleep_SleepKey")) // Triggers the lie-down emote if the player can lie down ingame
-		{
-			bool canSleep = player.GetEmoteManager().CanPlayEmote(EmoteConstants.ID_EMOTE_LYINGDOWN);
+        if (chfInput.LocalPress("UACHFSleep_RefreshConfig")) // Sends request to server to reload json config if player is admin
+        {
+            player.RequestServerConfigReload();
+        } else
+        if (chfInput.LocalPress("UACHFSleep_SleepKey")) // Triggers the lie-down emote if the player can lie down ingame
+        {
+            bool canSleep = player.GetEmoteManager().CanPlayEmote(EmoteConstants.ID_EMOTE_LYINGDOWN);
 
-			if (canSleep)
-			{
-				if (player.GetEmoteManager())
-				{
-					player.GetEmoteManager().CreateEmoteCBFromMenu(EmoteConstants.ID_EMOTE_LYINGDOWN);
-				}
-			}
-		}
-	}
-}
+            if (canSleep)
+            {
+                if (player.GetEmoteManager())
+                {
+                    player.GetEmoteManager().CreateEmoteCBFromMenu(EmoteConstants.ID_EMOTE_LYINGDOWN);
+                }
+            }
+        }
+    }
+};
+
+// vim:ft=enforce
